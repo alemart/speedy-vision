0d3ea0e3cda17af1cb60d6b1a9774966e1b5c25c
alemart <alemartf@gmail.com>
2020-05-05 13:29:53 -0300

    Added gpu-js.min

--- src/gpu/gpu-kernels.js
+++ src/gpu/gpu-kernels.js
@@ -19,7 +19,7 @@
  * The set of all GPU kernel groups for accelerated computer vision
  */
 
-import { GPU } from './gpu-browser';
+const { GPU } = require(process.env.NODE_ENV == 'development' ? './gpu-js/gpu-browser' : './gpu-js/gpu-browser.min');
 import { Utils } from '../utils/utils';
 import { GPUColors } from './gpu-colors';
 import { GPUFilters } from './gpu-filters';
