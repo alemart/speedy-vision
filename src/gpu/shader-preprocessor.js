20ddd30868d1feb1d53f2e405edee9c2d0c79ca6
alemart <alemartf@gmail.com>
2020-07-08 15:50:13 -0300

    Fix

--- src/gpu/shader-preprocessor.js
+++ src/gpu/shader-preprocessor.js
@@ -60,7 +60,6 @@ export class ShaderPreprocessor
             return require('./kernels/shaders/includes/' + filename);
 
         throw GLUtils.Error(`Shader preprocessor: can't read file \"${filename}\"`);
-        return '';
     }
 }
 
