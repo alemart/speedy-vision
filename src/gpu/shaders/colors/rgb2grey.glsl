7e17ed50c0c53e9e63251bc08c8670034eeccfbb
alemart <alemartf@gmail.com>
2020-07-09 16:30:30 -0300

    Added headers

--- src/gpu/shaders/colors/rgb2grey.glsl
+++ src/gpu/shaders/colors/rgb2grey.glsl
@@ -1,7 +1,27 @@
+/*
+ * speedy-vision.js
+ * GPU-accelerated Computer Vision for the web
+ * Copyright 2020 Alexandre Martins <alemartf(at)gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * rgb2grey.glsl
+ * Convert image to greyscale
+ */
+
 const vec4 grey = vec4(0.299f, 0.587f, 0.114f, 0.0f);
 uniform sampler2D image;
 
-// Convert to greyscale
 void main()
 {
     vec4 pixel = threadPixel(image);
--- src/gpu/shaders/keypoint-detectors/fast7.glsl
+++ src/gpu/shaders/keypoint-detectors/fast7.glsl
@@ -1,8 +1,29 @@
+/*
+ * speedy-vision.js
+ * GPU-accelerated Computer Vision for the web
+ * Copyright 2020 Alexandre Martins <alemartf(at)gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * fast7.glsl
+ * FAST-7,12 corner detector
+ */
+
 uniform sampler2D image;
 uniform float threshold;
 
-// FAST-5_8: requires 5 contiguous pixels
-// on a circumference of 8 pixels
+// FAST-7,12: requires 7 contiguous pixels
+// on a circumference of 12 pixels
 void main()
 {
     ivec2 thread = threadLocation();
