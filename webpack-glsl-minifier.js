5b9f1a72ceba6e74f4ebb868f948c1134120d4f0
alemart <alemartf@gmail.com>
2020-08-05 19:05:21 -0300

    Small changes

--- webpack-glsl-minifier.js
+++ webpack-glsl-minifier.js
@@ -24,15 +24,15 @@ function minify(glslSource)
     const multilineComments = /\/\*(.|\s)*?\*\//g;
     const lineComments = /\/\/.*$/gm;
 
-    glslSource = glslSource
-                .replace(multilineComments, '')
-                .replace(lineComments, '')
-                .split('\n')
-                .map(line => line.trim())
-                .filter(line => line)
-                .join('\n');
+    const minifiedSource = glslSource
+                           .replace(multilineComments, '')
+                           .replace(lineComments, '')
+                           .split('\n')
+                           .map(line => line.trim())
+                           .filter(line => line)
+                           .join('\n');
 
-    return 'module.exports = ' + JSON.stringify((glslSource));
+    return 'module.exports = ' + JSON.stringify(minifiedSource);
 }
 
 module.exports = minify;
