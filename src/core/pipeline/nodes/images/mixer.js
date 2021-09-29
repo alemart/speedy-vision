2aadfe2c990df27643f49009db89c3972c95d243
alemart <alemartf@gmail.com>
2021-09-28 22:22:46 -0300

    Add Speedy.Image.Mixer()

---
+++ src/core/pipeline/nodes/images/mixer.js
@@ -0,0 +1,137 @@
+/*
+ * speedy-vision.js
+ * GPU-accelerated Computer Vision for JavaScript
+ * Copyright 2020-2021 Alexandre Martins <alemartf(at)gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * mixer.js
+ * Image Mixer
+ */
+
+import { SpeedyPipelineNode } from '../../pipeline-node';
+import { SpeedyPipelineMessageType, SpeedyPipelineMessageWithImage } from '../../pipeline-message';
+import { InputPort, OutputPort } from '../../pipeline-portbuilder';
+import { SpeedyGPU } from '../../../../gpu/speedy-gpu';
+import { SpeedyTexture } from '../../../../gpu/speedy-texture';
+import { Utils } from '../../../../utils/utils';
+import { ImageFormat } from '../../../../utils/types';
+import { SpeedyPromise } from '../../../../utils/speedy-promise';
+import { NotSupportedError } from '../../../../utils/errors';
+
+/**
+ * Image Mixer
+ */
+export class SpeedyPipelineNodeImageMixer extends SpeedyPipelineNode
+{
+    /**
+     * Constructor
+     * @param {string} [name] name of the node
+     */
+    constructor(name = undefined)
+    {
+        super(name, 1, [
+            InputPort('in0').expects(SpeedyPipelineMessageType.Image),
+            InputPort('in1').expects(SpeedyPipelineMessageType.Image),
+            OutputPort().expects(SpeedyPipelineMessageType.Image),
+        ]);
+
+        /** @type {number} alpha coefficient (applied to image0) */
+        this._alpha = 0.5;
+
+        /** @type {number} beta coefficient (applied to image1) */
+        this._beta = 0.5;
+
+        /** @type {number} gamma coefficient (brightness control) */
+        this._gamma = 0.0;
+    }
+
+    /**
+     * Alpha coefficient (applied to image0)
+     * @returns {number}
+     */
+    get alpha()
+    {
+        return this._alpha;
+    }
+
+    /**
+     * Alpha coefficient (applied to image0)
+     * @param {number} value
+     */
+    set alpha(value)
+    {
+        this._alpha = +value;
+    }
+
+    /**
+     * Beta coefficient (applied to image1)
+     * @returns {number}
+     */
+    get beta()
+    {
+        return this._beta;
+    }
+
+    /**
+     * Beta coefficient (applied to image1)
+     * @param {number} value
+     */
+    set beta(value)
+    {
+        this._beta = +value;
+    }
+
+    /**
+     * Gamma coefficient (brightness control)
+     * @returns {number}
+     */
+    get gamma()
+    {
+        return this._gamma;
+    }
+
+    /**
+     * Gamma coefficient (brightness control)
+     * @param {number} value
+     */
+    set gamma(value)
+    {
+        this._gamma = +value;
+    }
+
+    /**
+     * Run the specific task of this node
+     * @param {SpeedyGPU} gpu
+     * @returns {void|SpeedyPromise<void>}
+     */
+    _run(gpu)
+    {
+        const in0 = this.input('in0').read();
+        const in1 = this.input('in1').read();
+        const image0 = in0.image, image1 = in1.image;
+        const format0 = in0.format, format1 = in1.format;
+        const width = Math.max(image0.width, image1.width);
+        const height = Math.max(image0.height, image1.height);
+        const alpha = this._alpha, beta = this._beta, gamma = this._gamma;
+        const outputTexture = this._tex[0];
+
+        if(format0 != format1)
+            throw new NotSupportedError(`Can't mix images of different formats`);
+
+        gpu.programs.transforms.additiveMix.outputs(width, height, outputTexture);
+        gpu.programs.transforms.additiveMix(image0, image1, alpha, beta, gamma);
+
+        this.output().swrite(outputTexture, format0);
+    }
+}
---
+++ src/gpu/shaders/transforms/additive-mix.glsl
@@ -0,0 +1,54 @@
+/*
+ * speedy-vision.js
+ * GPU-accelerated Computer Vision for JavaScript
+ * Copyright 2020-2021 Alexandre Martins <alemartf(at)gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * additive-mix.glsl
+ * Additive mix of two images (blend)
+ */
+
+@include "subpixel.glsl"
+
+uniform sampler2D image0;
+uniform sampler2D image1;
+uniform float alpha;
+uniform float beta;
+uniform float gamma;
+
+/*
+
+The resulting image is computed as follows:
+
+Image = alpha * image0 + beta * image1 + gamma
+
+If you pick an alpha in [0,1] and set beta = alpha and gamma = 0,
+you get a nice alpha blending effect
+
+*/
+
+const vec4 BACKGROUND = vec4(0.0f); // this vector can't be scaled
+
+void main()
+{
+    ivec2 location = threadLocation();
+    ivec2 size0 = textureSize(image0, 0);
+    ivec2 size1 = textureSize(image1, 0);
+
+    vec4 pix0 = all(lessThan(location, size0)) ? pixelAt(image0, location) : BACKGROUND;
+    vec4 pix1 = all(lessThan(location, size1)) ? pixelAt(image1, location) : BACKGROUND;
+
+    vec4 pix = clamp(alpha * pix0 + beta * pix1 + gamma, 0.0f, 1.0f);
+    color = vec4(pix.rgb, 1.0f);
+}
