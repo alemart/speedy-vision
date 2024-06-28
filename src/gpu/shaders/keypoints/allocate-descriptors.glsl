cbaf44c96bb9067c19d47e74ef84968ac9461336
alemart <221477+alemart@users.noreply.github.com>
2024-06-28 11:39:05 -0300

    Update year

--- src/gpu/shaders/keypoints/allocate-descriptors.glsl
+++ src/gpu/shaders/keypoints/allocate-descriptors.glsl
@@ -1,7 +1,7 @@
 /*
  * speedy-vision.js
  * GPU-accelerated Computer Vision for JavaScript
- * Copyright 2020-2023 Alexandre Martins <alemartf(at)gmail.com>
+ * Copyright 2020-2024 Alexandre Martins <alemartf(at)gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
